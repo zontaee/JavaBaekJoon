a415af9a1f3762a08eef212da1d4d067f4c62a00
zontaee <whdlsxo123@naver.com>
2021-10-31 22:56:59 +0900

    2021.10.31

---
+++ src/Char/Char_11654.java
@@ -0,0 +1,15 @@
+package Char;
+
+import java.util.Scanner;
+
+public class Char_11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String b= sc.next();
+        char a = b.charAt(0);
+
+
+        System.out.println((int)a);
+    }
+}
