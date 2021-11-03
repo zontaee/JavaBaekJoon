829600cf4211ed16b5c31d0ba86184851908a1c2
zontaee <whdlsxo123@naver.com>
2021-11-03 21:29:47 +0900

    2021.11.03

---
+++ src/Array/Char_2675.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Char_2675 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        for(int i = 0; i < a; i++) {
+
+            int b= in.nextInt();
+            String str = in.next();
+
+            for(int j = 0; j < str.length(); j++) {
+                for(int k = 0; k < b; k++) {
+                    System.out.print(str.charAt(j));
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
